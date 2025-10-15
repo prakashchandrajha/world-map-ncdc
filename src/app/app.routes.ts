3f8c00ca282b0719b1aeeff7c1f408fa8adda96d
Your Name <your.email@example.com>
2025-10-15 15:42:15 +0530

    minor change

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { About } from './components/about/about';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:About}
+];

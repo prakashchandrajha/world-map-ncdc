dea41982561b3405f838e887fa8f0abb14fb2c3f
Your Name <your.email@example.com>
2025-10-24 17:30:53 +0530

    change design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { About } from './components/about/about';
 import { Home } from './components/home/home';
+import { TheList } from './components/the-list/the-list';
 
 export const routes: Routes = [
     {path:'',component:Home},
-    {path:'about',component:About}
+    {path:'about',component:About},
+    {path:'list',component:TheList}
 ];
---
+++ src/app/components/the-list/the-list.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Banner } from "../../shared/banner/banner";
+
+@Component({
+  selector: 'app-the-list',
+  imports: [Banner],
+  templateUrl: './the-list.html',
+  styleUrl: './the-list.css'
+})
+export class TheList {
+
+}

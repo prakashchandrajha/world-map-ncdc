fdb1bafb9d93de482803fd03d4beb835776465f1
prakash <prakashchandrajhaideal@gmail.com>
2025-11-05 12:38:15 +0530

    ADD SOME MORE POINTS ACTIVE ON WORLD MAP 5NOV

--- src/app/components/world-map/world-map.component.ts
+++ src/app/components/world-map/world-map.component.ts
@@ -112,21 +112,21 @@ constructor(private router: Router) {}
   { id: 'amal-cooperative-tamanar', name: 'Amal Cooperative (Women’s cooperatives in the Argan sector)', lat: 31.4000, lng: -9.7000, country: 'Morocco', continent: 'Africa' },
   { id: 'amul-dairy', name: 'Amul Dairy (Kaira District Milk Union)', lat: 22.55267, lng: 72.9715, country: 'India', continent: 'Asia' },
   { id: 'americas-credit-union-museum', name: 'America’s Credit Union Museum', lat: 42.9933, lng: -71.4758, country: 'USA', continent: 'North America' },
-  { id: 'greenbelt-museum', name: 'Greenbelt Museum & Housing Cooperative', lat: 38.998318, lng: -76.896332, country: 'USA', continent: 'North America' },
+  // { id: 'greenbelt-museum', name: 'Greenbelt Museum & Housing Cooperative', lat: 38.998318, lng: -76.896332, country: 'USA', continent: 'North America' },
   { id: 'moshi-cooperative-university', name: 'Moshi Cooperative University (MoCU)', lat: -3.3400, lng: 37.3500, country: 'Tanzania', continent: 'Africa' },
   { id: 'sunchales-monumento-cooperativismo', name: 'Sunchales – Monumento Nacional al Cooperativismo', lat: -31.633, lng: -61.500, country: 'Argentina', continent: 'South America' },
   { id: 'theodor-amstad-monument', name: 'Theodor Amstad Square and Monument to Cooperativism', lat: -29.3804, lng: -51.1087, country: 'Brazil', continent: 'South America' },
   { id: 'national-cooperative-museum', name: 'National Co-operative Museum', lat: 51.9156, lng: 4.3970, country: 'Netherlands', continent: 'Europe' },
-  { id: 'samuel-jurkovic-museum', name: 'The Co-operative Museum of Samuel Jurkovič', lat: 48.4917, lng: 17.7050, country: 'Slovak Republic', continent: 'Europe' },
+  // { id: 'samuel-jurkovic-museum', name: 'The Co-operative Museum of Samuel Jurkovič', lat: 48.4917, lng: 17.7050, country: 'Slovak Republic', continent: 'Europe' },
   { id: 'obisesan-building', name: 'Obisesan Building/Hall', lat: 7.3964, lng: 3.9167, country: 'Nigeria', continent: 'Africa' },
   { id: 'naruto-toyohiko-memorial-museum', name: 'Naruto City Kagawa Toyohiko Memorial Museum', lat: 34.1633, lng: 134.4988, country: 'Japan', continent: 'Asia' },
   { id: 'maktab-koperasi-malaysia', name: 'Maktab Koperasi Malaysia (Alor Setar branch)', lat: 6.1179, lng: 100.3669, country: 'Malaysia', continent: 'Asia' },
   { id: 'cooperative-university-kenya', name: 'The Co-operative University of Kenya', lat: -1.2804, lng: 36.8163, country: 'Kenya', continent: 'Africa' },
   { id: 'adamant-food-coop', name: 'Adamant Food Co-op', lat: 44.3341, lng: -72.7790, country: 'USA', continent: 'North America' },
-  { id: 'uralungal-labour-contract-cooperative', name: 'Uralungal Labour Contract Cooperative Headquarters', lat: 11.6094, lng: 75.6150, country: 'India', continent: 'Asia' },
-  { id: 'workers-assembly-halls-denmark', name: 'Workers’ Assembly Halls', lat: 55.684833, lng: 12.570250, country: 'Denmark', continent: 'Europe' },
+  // { id: 'uralungal-labour-contract-cooperative', name: 'Uralungal Labour Contract Cooperative Headquarters', lat: 11.6094, lng: 75.6150, country: 'India', continent: 'Asia' },
+  // { id: 'workers-assembly-halls-denmark', name: 'Workers’ Assembly Halls', lat: 55.684833, lng: 12.570250, country: 'Denmark', continent: 'Europe' },
   { id: 'levigliani-village', name: 'The Village of Levigliani', lat: 44.0722, lng: 10.2387, country: 'Italy', continent: 'Europe' },
-  { id: 'marinaleda-cooperative', name: 'Marinaleda Cooperative', lat: 37.3292, lng: -5.0161, country: 'Spain', continent: 'Europe' },
+  // { id: 'marinaleda-cooperative', name: 'Marinaleda Cooperative', lat: 37.3292, lng: -5.0161, country: 'Spain', continent: 'Europe' },
   { id: 'rochdale-pioneer-museum', name: 'Rochdale Pioneer Museum and Heritage Trust', lat: 53.6141, lng: -2.1561, country: 'UK', continent: 'Europe' },
   { id: 'kibbutz-degania-alef', name: 'Kibbutz Degania Alef', lat: 32.8154, lng: 35.5630, country: 'Israel', continent: 'Asia' }
 ];
@@ -140,7 +140,7 @@ constructor(private router: Router) {}
 <p class="text-sm text-gray-600 mb-1">
   <span class="font-medium text-gray-700">Country:</span> ${location.country}
 </p>
-<img src="${location.img}" alt="${location.name}" class="w-full h-32 object-cover rounded-lg" />
+
 
   <button
     class=\"view-details-btn mt-4 bg-red-500 hover:bg-red-700 text-white font-semibold py-2 px-4 rounded transition duration-300 ease-in-out\"
